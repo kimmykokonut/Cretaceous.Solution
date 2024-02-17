502584f71b4f719c49372c432aeb3e9f3ed36415
Kim Robinson <kimmykokonut@gmail.com>
2024-02-17 14:53:42 -0800

    seed database with 5 animals

--- CretaceousApi/Models/CretaceousApiContext.cs
+++ CretaceousApi/Models/CretaceousApiContext.cs
@@ -8,4 +8,16 @@ public class CretaceousApiContext : DbContext
   public CretaceousApiContext(DbContextOptions<CretaceousApiContext> options) : base(options)
   {
   }
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    builder.Entity<Animal>()
+      .HasData(
+        new Animal { AnimalId = 1, Name = "Matilda", Species = "Wooly Mammoth", Age = 7 },
+        new Animal { AnimalId = 2, Name = "Rexie", Species = "Dinosaur", Age = 10 },
+        new Animal { AnimalId = 3, Name = "Matilda", Species = "Dinosaur", Age = 2 },
+        new Animal { AnimalId = 4, Name = "Pip", Species = "Shark", Age = 4 },
+        new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Dinosaur", Age = 22 }
+      );
+
+  }
 }
